03d7f882e4a681f0f9d84bcc71778518e8853b9e
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2024-12-27 08:18:11 +0100

    react learning

--- ems.ui/src/App.tsx
+++ ems.ui/src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import "primeflex/primeflex.css";
 import Task from "./Components/Task/Task.tsx";
 import EmployeeList from "./Components/EmployeeList/EmployeeList.tsx";
+import Search from "./Components/Search/Search.tsx";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <EmployeeList />
     </div>
   );
