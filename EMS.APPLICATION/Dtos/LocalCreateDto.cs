049fa7779dcbb9dfb999dc0d5e7d48d6d2996aba
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2025-04-30 07:00:09 +0200

    Removed usings and added required disabel to button.

--- EMS.APPLICATION/Dtos/LocalCreateDto.cs
+++ EMS.APPLICATION/Dtos/LocalCreateDto.cs
@@ -1,11 +1,4 @@
-﻿using EMS.CORE.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EMS.APPLICATION.Dtos
+﻿namespace EMS.APPLICATION.Dtos
 {
     public class LocalCreateDto
     {
--- EMS.APPLICATION/Dtos/TaskGetDto.cs
+++ EMS.APPLICATION/Dtos/TaskGetDto.cs
@@ -1,9 +1,4 @@
 ﻿using EMS.CORE.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EMS.APPLICATION.Dtos
 {
