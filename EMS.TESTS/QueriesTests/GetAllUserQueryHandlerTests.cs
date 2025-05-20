02f2e2ae9dd11c4c69f4776a968a5219dc8dbf11
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2025-05-20 06:58:08 +0200

    Added  GetAllUserQueryHandlerTests.

---
+++ EMS.TESTS/QueriesTests/GetAllUserQueryHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using EMS.APPLICATION.Features.Account.Queries;
+using EMS.CORE.Entities;
+using EMS.CORE.Interfaces;
+using EMS.INFRASTRUCTURE.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EMS.TESTS.QueriesTests
+{
+    [TestClass]
+    public class GetAllUserQueryHandlerTests
+    {
+        private readonly Mock<IUserRepository> _mockUserRepository;
+        private readonly GetAllUserQueryHandler _handler;
+
+        public GetAllUserQueryHandlerTests()
+        {
+            _mockUserRepository = new Mock<IUserRepository>();
+            _handler = new GetAllUserQueryHandler(_mockUserRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task Handle_Returns_Users()
+        {
+            // Arrange
+            int pageNumber = 1;
+            int pageSize = 10;
+
+            var expectedUsers = new List<AppUserEntity>
+            {
+               new AppUserEntity { UserName = "John", Email = "john@example.com" },
+               new AppUserEntity { UserName = "Johnny", Email = "johnny@example.com" }
+            };
+
+            var expectedResult = new PaginatedList<AppUserEntity>(expectedUsers, expectedUsers.Count, pageNumber, pageSize);
+
+            _mockUserRepository.Setup(repo => repo.GettAllUsersAsync(pageNumber, pageSize, null))
+                .ReturnsAsync(expectedResult);
+
+            var query = new GetAllUserQuery(pageNumber, pageSize, null);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedUsers.Count, result.Items.Count);
+            Assert.AreEqual(expectedUsers[0].Email, result.Items[0].Email);
+        }
+    }
+}
