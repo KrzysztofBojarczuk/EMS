70cb08ebd48b30dc71fd9dbb3f8dc9064f37f500
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2025-08-01 07:04:25 +0200

    Added test handler GetUserNumberOfEmployeesQueryHandlerTests.

---
+++ EMS.TESTS/FeaturesTests/EmployeeTests/QueriesTests/GetUserNumberOfEmployeesQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using EMS.APPLICATION.Features.Employee.Queries;
+using EMS.CORE.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EMS.TESTS.FeaturesTests.EmployeeTests.QueriesTests
+{
+    [TestClass]
+    public class GetUserNumberOfEmployeesQueryHandlerTests
+    {
+        private Mock<IEmployeeRepository> _mockEmployeeRepository;
+        private GetUserNumberOfEmployeesQueryHandler _handler;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            _handler = new GetUserNumberOfEmployeesQueryHandler(_mockEmployeeRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task Handle_Returns_NumberOfUserEmployees()
+        {
+            // Arrange
+            var appUserId = "user-id-123";
+            var expectedCount = 3;
+
+            _mockEmployeeRepository.Setup(x => x.GetUserNumberOfEmployeesAsync(appUserId))
+                .ReturnsAsync(expectedCount);
+
+            var query = new GetUserNumberOfEmployeesQuery(appUserId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(expectedCount, result);
+            _mockEmployeeRepository.Verify(x => x.GetUserNumberOfEmployeesAsync(appUserId), Times.Once);
+        }
+    }
+}
