f5f45d8e71e28e9676b4eeddfb5a3514ce8dfb57
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2025-11-01 08:10:49 +0100

    Added entity and interface and repository.

---
+++ EMS.CORE/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,16 @@
+﻿using EMS.CORE.Entities;
+using EMS.CORE.Enums;
+using EMS.INFRASTRUCTURE.Extensions;
+
+namespace EMS.CORE.Interfaces
+{
+    public interface IVehicleRepository
+    {
+        Task<VehicleEntity> AddAVehicleAsync(VehicleEntity entity);
+        Task<VehicleEntity> GetVehicleByIdAsync(Guid vehicleId);
+        Task<VehicleEntity> UpdateVehicleAsync(Guid vehicleId, string appUserId, VehicleEntity entity);
+        Task<PaginatedList<VehicleEntity>> GetUserVehiclesAsync(string appUserId, int pageNumber, int pageSize, List<VehicleType> vehicleType, string searchTerm);
+        Task<IEnumerable<VehicleEntity>> GetUserVehiclesForTaskAsync(string appUserId, string searchTerm);
+        Task<bool> DeleteVehicleAsync(Guid vehicleId, string appUserId);
+    }
+}
---
+++ EMS.INFRASTRUCTURE/Repositories/VehicleRepository.cs
@@ -0,0 +1,45 @@
+﻿using EMS.CORE.Entities;
+using EMS.CORE.Enums;
+using EMS.CORE.Interfaces;
+using EMS.INFRASTRUCTURE.Data;
+using EMS.INFRASTRUCTURE.Extensions;
+
+namespace EMS.INFRASTRUCTURE.Repositories
+{
+    public class VehicleRepository(AppDbContext dbContext) : IVehicleRepository
+    {
+        public async Task<VehicleEntity> AddAVehicleAsync(VehicleEntity entity)
+        {
+            entity.Id = Guid.NewGuid(); //służy do przypisania nowego, unikalnego identyfikatora
+            dbContext.Vehicles.Add(entity);
+
+            await dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+        public Task<bool> DeleteVehicleAsync(Guid vehicleId, string appUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaginatedList<VehicleEntity>> GetUserVehiclesAsync(string appUserId, int pageNumber, int pageSize, List<VehicleType> vehicleType, string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VehicleEntity>> GetUserVehiclesForTaskAsync(string appUserId, string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleEntity> GetVehicleByIdAsync(Guid vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleEntity> UpdateVehicleAsync(Guid vehicleId, string appUserId, VehicleEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
