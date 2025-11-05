9ebaa89d2cae199d994a1611280bdfa932ffcdb0
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2025-11-05 06:50:38 +0100

    Added parametr in vehicle in method controller, handler and repository.

--- EMS.APPLICATION/Features/Vehicle/Queries/GetUserVehiclesQuery.cs
+++ EMS.APPLICATION/Features/Vehicle/Queries/GetUserVehiclesQuery.cs
@@ -6,13 +6,13 @@ using MediatR;
 
 namespace EMS.APPLICATION.Features.Vehicle.Queries
 {
-    public record GetUserVehiclesQuery(string appUserId, int pageNumber, int pageSize, List<VehicleType> vehicleType, string searchTerm) : IRequest<PaginatedList<VehicleEntity>>;
+    public record GetUserVehiclesQuery(string appUserId, int pageNumber, int pageSize, List<VehicleType> vehicleType, string searchTerm, DateTime? dateFrom, DateTime? dateTo, string sortOrderDate, string sortOrderMileage) : IRequest<PaginatedList<VehicleEntity>>;
 
     public class GetUserVehiclesQueryHandler(IVehicleRepository vehicleRepository) : IRequestHandler<GetUserVehiclesQuery, PaginatedList<VehicleEntity>>
     {
         public async Task<PaginatedList<VehicleEntity>> Handle(GetUserVehiclesQuery request, CancellationToken cancellationToken)
         {
-           return await vehicleRepository.GetUserVehiclesAsync(request.appUserId, request.pageNumber, request.pageSize, request.vehicleType, request.searchTerm);
+           return await vehicleRepository.GetUserVehiclesAsync(request.appUserId, request.pageNumber, request.pageSize, request.vehicleType, request.searchTerm, request.dateFrom, request.dateTo, request.sortOrderDate, request.sortOrderMileage);
         }
     }
 }
