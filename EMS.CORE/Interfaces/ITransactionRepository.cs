264c5440509aaca530d12976d08eddd478d414e6
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2026-04-03 06:48:30 +0200

    Added  method GetTransactionByIdAsync in TransactionRepository.

--- EMS.CORE/Interfaces/ITransactionRepository.cs
+++ EMS.CORE/Interfaces/ITransactionRepository.cs
@@ -6,6 +6,7 @@ namespace EMS.CORE.Interfaces
     public interface ITransactionRepository
     {
         Task<TransactionEntity> AddTransactionAsync(TransactionEntity entity);
+        Task<TransactionEntity> GetTransactionByIdAsync(Guid id);
         Task<IEnumerable<TransactionEntity>> GetTransactionsByBudgetIdAsync(Guid id, string searchTerm, List<CategoryType> category, DateTime? dateFrom, DateTime? dateTo, decimal? amountFrom, decimal? amountTo, string sortOrder);
         Task<bool> DeleteTransactionsAsync(Guid transactionId);
     }
